82007a96c6db636358e5d63bf50586b29d0feea7
Yoanna <yoanna.yordanova2402@gmail.com>
2025-02-20 14:35:22 +0200

    battle game with warriors

---
+++ spreadsheets_reader/week8/class_mage.py
@@ -0,0 +1,12 @@
+from class_character import Character
+
+class Mage(Character):
+    def __init__(self, name, health, attack_power, defense):
+        Character.__init__(self,name,health,attack_power,defense)
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+        self.defense = defense
+
+    def abillity(self):
+        self.attack_power = self.attack_power*2
