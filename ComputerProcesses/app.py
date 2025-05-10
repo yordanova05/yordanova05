49c08cabd12f81528231776d778ffffb1b272677
Yoanna Yordanova <yoanna.yordanova2402@gmail.com>
2025-05-10 15:39:18 +0300

    Working code!!!

--- ComputerProcesses/app.py
+++ ComputerProcesses/app.py
@@ -8,7 +8,7 @@ def home():
     return render_template("index.html")
 
 @app.route("/process")
-def process_status(filter_name = None, sort_by = None, order = "asc", ppid = None):
+def process_status():
     
     filter_name = request.args.get("name",default=None,type=str)
     sort_by = request.args.get("sort", default=None, type=str)
@@ -16,41 +16,46 @@ def process_status(filter_name = None, sort_by = None, order = "asc", ppid = Non
     ppid = request.args.get("ppid", default=None, type= int)
     
     process_list = []
-    process_settings = ["pid","name","cpu_percent","memory_percent","status","ppid"]
     anomalies = []
+    process_settings = ["pid","name","cpu_percent","memory_percent","status","ppid"]
+
     cpu_max = 85.0
     memory_max = 80.0
     
     for p in psu.process_iter(process_settings):
         try:
             info = p.info
+            info["cpu_percent"] = round(info["cpu_percent"], 2)
+            info["memory_percent"] = round(info["memory_percent"], 2)
             
             if info["name"] == "System Idle Process" or info["pid"] == 0:
                 continue
+            
+            if info["cpu_percent"] >= cpu_max or info["memory_percent"] >= memory_max:
+                anomalies.append(info["pid"])
         
             if filter_name and filter_name.lower() not in info["name"].lower():
                 continue
             
             if ppid and info["ppid"] != ppid:
                 continue
-            
-            if info["cpu_percent"] >= cpu_max or info["memory_percent"] >= memory_max:
-                
-                anomalies.append(p.name())
-            
+
             process_list.append(info)
                 
         except(psu.NoSuchProcess, psu.AccessDenied):
             continue
         
     
-    if sort_by in info:
+    if process_list and sort_by in info:
         if order.lower() == "desc":
             process_list.sort(key = lambda x: x[sort_by], reverse = True)
         elif order.lower() == "asc":
             process_list.sort(key = lambda x: x[sort_by], reverse = False)
         
-    return jsonify(process_list, anomalies)
+    if not process_list:
+        return jsonify({"message": "No data with these parameters!"})
+    
+    return jsonify({"processes":process_list, "anomalies":anomalies})
 
 if __name__ == "__main__":
     
--- ComputerProcesses/static/script.js
+++ ComputerProcesses/static/script.js
@@ -1,11 +1,13 @@
-function data(){
+let refreshTimeout = null;
+function data_analyze(){
+
     const seconds = document.getElementById("seconds").value;
     const name = document.getElementById("filtername").value;
     const sort = document.getElementById("sorted").value;
     const order = document.getElementById("order").value;
     const ppid = document.getElementById("ppid").value;
 
-    const url = `/process?seconds=${seconds}&name=${name}&sort=${sort}&order=${order}&ppid=${ppid}`
+    const url = `/process?name=${name}&sort=${sort}&order=${order}&ppid=${ppid}`
 
     fetch(url)
         .then(response => response.json())
@@ -13,23 +15,36 @@ function data(){
             const tbody = document.getElementById("table-body");
             tbody.innerHTML = ""; // Изчистване на предишни редове
 
-            data.forEach(p => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
-                    <td>${p.pid}</td>
-                    <td>${p.name}</td>
-                    <td>${p.cpu_percent}</td>
-                    <td>${p.memory_percent}</td>
-                    <td>${p.status}</td>
-                    <td>${p.ppid}</td>
-                `;
-                tbody.appendChild(row);
-            });
+            if ( data.processes ){
+                    data.processes.forEach(p => {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                        <td>${p.pid}</td>
+                        <td>${p.name}</td>
+                        <td>${p.cpu_percent}</td>
+                        <td>${p.memory_percent}</td>
+                        <td>${p.status}</td>
+                        <td>${p.ppid}</td>
+                    `;
+                    if (data.anomalies.includes(p.pid)){
+                        row.classList.add("anomal-class");
+                    }
+                    tbody.appendChild(row);
+                });
+                if ( seconds > 0 ){
+                    refreshTimeout = setTimeout(data_analyze, seconds * 1000);
+                } 
+            }
+            else{
+                const message = document.createElement("h3");
+                message.innerHTML = data.message
+                message.classList.add("no-data-message");
+                document.body.appendChild(message);
+                return;
+            }
         });
-
-    if ( seconds > 0 ){
-        refreshTimeout = setTimeout(data, seconds * 1000);
-    }
+        
+    
 }
 
 function clear_all(){
@@ -39,16 +54,20 @@ function clear_all(){
     document.getElementById("order").value = "asc";
     document.getElementById("ppid").value = "";
 
+    const message = document.querySelector(".no-data-message");
+    if (message) {
+        message.remove();
+    }
+
+    if (refreshTimeout){
+        clearTimeout(refreshTimeout);
+        refreshTimeout = null;
+    }
     document.getElementById("table-body").innerHTML = "";
-        if (refreshTimeout){
-            clearTimeout(refreshTimeout);
-            refreshTimeout = null;
-        }
+        
 }
 
 window.onload = () => {
-    isClear = false;
-    document.getElementById("result").addEventListener("click", data)
-
+    document.getElementById("result").addEventListener("click", data_analyze)
     document.getElementById("clear").addEventListener("click",clear_all)
 }
